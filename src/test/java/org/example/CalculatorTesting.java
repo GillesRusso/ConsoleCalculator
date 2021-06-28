b4e8cd33a428a127e62dc98d358b4d804e17724b
Cieluu <russogilles5@gmail.com>
2021-06-28 14:31:07 +0200

    zwei Test mit Min und Max Value hinzugefügt

--- src/test/java/org/example/CalculatorTesting.java
+++ src/test/java/org/example/CalculatorTesting.java
@@ -22,6 +22,15 @@ public class CalculatorTesting {
     public void testSubtractionZweiPositiveIsOk(){
         assertTrue(calculator.subtraktion(20, 10)== 10);
     }
+    @Test
+    public void testMaxValueSumme(){
+        assertFalse(calculator.summe(Integer.MAX_VALUE, Integer.MAX_VALUE)==1);
+    }
+    @Test
+    public void testMinValueSubtraktion(){
+        assertFalse(calculator.summe(Integer.MIN_VALUE, Integer.MIN_VALUE)==1);
+    }
+
     @After
     public void teardown(){
         calculator.reset(0,0);
